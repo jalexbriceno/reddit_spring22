f0fca77d35cad4d5ee4681a51fadac04b99cc051
Alex Briceno <jalexbriceno@gmail.com>
2022-03-07 20:55:11 -0700

    comment controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,81 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/") only for rails view
+  # Defines the root path route ("/") only for rails views
   # root "articles#index"
 
+  # API Routes
+  # a way to link up to the controller
+  # react side to use 
 
-# Api routes
-# a way to link up to the controller
-# react side to use
+  # backend as a api and also the controllers will be in the api folder
+  # http request 
+  # communicate in the browser
+  # have react communicate to the rails side using these routes 
+  # GET  -> READ
+  # POST  -> Create
+  # PUT / PATCH -> update
+  # DELETE  -> destroy 
+  # namespace :api do 
+    # where all our routes will be in 
+    # http verb 'url path', to: 'controller#action'
+    # get '/people', to: 'people#index'
 
-# backend as a api and also the controllers with
-#htpp reques
-#communicate in the browser
-# have react communicate to the rails side using these routes
-# #
-namespace :api do
-# http verb 'url path', to: 'controller#action', as: 
-# get '/people', to: 'peope#index'
+    # this will create all of the routes for all base crud actions
+    # resources :subs do 
+    #   resources :topics
+    # end
+
+    # do not do this for 3 levels 
+    # resources :subs do 
+    #   resources :topics do 
+    #     resources :comments do
+    #       resources :likes
+    #     end
+    #   end
+    # end
+
+    # resources :topics, except: [:index, :show, :update, :create, :destroy] do 
+    #   resources :comments
+    # end
+
+    # resources :comments, except: [:index, :show, :update, :create, :destroy] do 
+    #   resources :likes
+    # end
 
 
+    # resources :subs, only: [:index, :show, :update]
+    # resources :subs, except: [:create, :destroy]
 
-# this will create all of the routes for all base crud actions
-resources :subs
-# resources :subs, only: [:index, :show, :update]
-# resources :subs, except: [:create, :destroy]
+    # if you want add a route with a model of parent and child
+    #  always go two level deep at most 
+    
+    # resources :parents do
+    #   resources :childs
+    # end
 
-# if you want add a route with a model of parent and child
-# always go two level deep at most
-# #   resources :parents do 
-# #     resources :childs
+    # resources :parents except: [:index, :show, :update, :create, :destroy] do
+      #   resources :childs
+      # end
 
-# resources :parents except: [:index :show, :update, :create, :destroy]
-end
+    # :id or :parent_id placeholder for the actual value
+    # /api/subs/1
+    # /api/subs/2
+    # /api/subs/3
+    # /api/subs/:id
 
+  # end
 
+  namespace :api do
+
+    resources :subs do 
+      resources :topics
+    end
+
+    resources :topics, except: [:index, :show, :update, :create, :destroy] do 
+      resources :comments
+    end
+    
+  end
 
 end
