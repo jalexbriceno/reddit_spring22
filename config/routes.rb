d1f9f00db59120fefb4dbd2a20fb5eb520ff6d0f
Alex Briceno <jalexbriceno@gmail.com>
2022-03-07 19:50:20 -0700

    sub controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,32 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  # Defines the root path route ("/") only for rails view
   # root "articles#index"
+
+
+# Api routes
+# a way to link up to the controller
+# react side to use
+
+# backend as a api and also the controllers with
+#htpp reques
+#communicate in the browser
+# have react communicate to the rails side using these routes
+# #
+namespace :api do
+# http verb 'url path', to: 'controller#action', as: 
+# get '/people', to: 'peope#index'
+
+
+
+# this will create all of the routes for all base crud actions
+resources :subs
+
+
+
+end
+
+
+
 end
